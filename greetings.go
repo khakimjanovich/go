b8c4f240c7a89e338af7a4237b29d1e161cce18f
Abduraxmanov Yunusali <asakadev@gmail.com>
2024-03-11 13:07:50 +0500

    updated for this branch

--- greetings.go
+++ greetings.go
@@ -1,19 +1,29 @@
 // Package greetings, Here we declared a package named greetings
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello : In Go,a function whose name starts with a capital letter
 // can be called by a function not in the same package. This is known
 // in Go as an exported name.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// In Go, the := operator is a shortcut for declaring and initializing a variable in one line
 	// (Go uses the value on the right to determine the variable's type). Taking the long way,
 	// you might have written this as:
 	//
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Wecome!", name)
 
-	return message
+	return message, nil
 }
